4269ff64dfbc78daaeb561091167f0837e208e5a
shvaknin <shirvaknin1234@gmail.com>
2017-11-21 21:56:53 +0200

    fgh

--- app/src/main/java/com/example/mailo/takengo/models/entities/Customer.java
+++ app/src/main/java/com/example/mailo/takengo/models/entities/Customer.java
@@ -5,7 +5,7 @@ package com.example.mailo.takengo.models.entities;
  */
 
 public class Customer {
-        String Name;
+        String familyName;
         String privateName;
         int idNum;
         int phoneNum;
