524752e94a4d54e7f583346ceb0a62f3deafc38c
shvaknin <shirvaknin1234@gmail.com>
2017-11-21 21:06:04 +0200

    bb

--- app/src/main/java/com/example/mailo/takengo/models/entities/Car.java
+++ app/src/main/java/com/example/mailo/takengo/models/entities/Car.java
@@ -8,7 +8,7 @@ public class Car {
     int branchNum;
     CarModel carModel;
     float kilometers;
-    int carNum;
+    double carNum;
 
     public Car() {//empty constructor
         this.branchNum =0 ;
