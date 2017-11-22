354b70a51b0bbb9f9b11a868d03f5ed5c4804294
shvaknin <shirvaknin1234@gmail.com>
2017-11-22 12:15:53 +0200

    "dsf"

--- app/src/main/java/com/example/mailo/takengo/models/entities/Address.java
+++ app/src/main/java/com/example/mailo/takengo/models/entities/Address.java
@@ -12,7 +12,7 @@ public class Address {
     public Address() {//empty Constructor
         this.city ="" ;
         this.street = "";
-        this.number = 0;
+        this.number = 4;
     }
     /**
      * Constructor
